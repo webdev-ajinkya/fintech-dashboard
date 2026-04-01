259fe9ee355e27f8397d00c40094e71c46361ce1
webdev-ajinkya <ajinkya.deshmukh.webdev@gmail.com>
2026-04-01 22:30:48 +0530

    setup: app folders created

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main>
+        <h1 className="flex justify-center text-3xl font-bold">App</h1>
+      </main>
+    </div>
+  );
+}
